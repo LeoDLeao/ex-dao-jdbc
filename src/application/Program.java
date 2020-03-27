eb9b9508ffbcca4a1f85e1dc8757d53e46b4f8c9
Leo Riegler <leo.riegler1@gmail.com>
2020-03-27 16:43:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		Department dp = new Department(1, "Books");
 		
-		Department dp = new Department(1,"Books");
+		Seller seller = new Seller(1,"Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+
+		System.out.println(seller);
 		
-		System.out.println(dp);
-		
-		
-		
+
 	}
 
 }
